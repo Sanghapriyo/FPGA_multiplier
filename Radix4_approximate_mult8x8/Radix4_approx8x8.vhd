97b465f5d7ad38ef49a97f7ae708b55012f04a9a
Sanghapriyo <sanghapriyo11@gmail.com>
2025-03-31 15:35:25 +0600

    Radix_approx8x8 improved area

--- Radix4_approximate_mult8x8/Radix4_approx8x8.vhd
+++ Radix4_approximate_mult8x8/Radix4_approx8x8.vhd
@@ -13,13 +13,12 @@ entity Radix4_approx8x8 is
 --        Sgen0,Sgen1,Sgen2,Sgen3: out STD_LOGIC_VECTOR(15 downto 0);
 --        Sa_comp : out STD_LOGIC_VECTOR(7 downto 0);
 --        Scomp0, Scomp1, Scomp2, Scomp3, Scomp4, Scomp5, Scomp6, Scomp7: out STD_LOGIC_VECTOR(1 downto 0);
---        Ssum0, Ssum1, Ssum2, Ssum3, Ssum4: out STD_LOGIC_VECTOR(3 downto 0);
 --        Scar: out STD_LOGIC_VECTOR(3 downto 0);
 --        Stest1, Stest2: out STD_LOGIC_VECTOR(14 downto 0);
 
         A,B  : in  STD_LOGIC_VECTOR(7 downto 0);
         prod : out STD_LOGIC_VECTOR(15 downto 0)
-);
+    );
 end Radix4_approx8x8;
 
 
@@ -31,17 +30,21 @@ signal a_comp : STD_LOGIC_VECTOR(7 downto 0);
 type pp_array0 is array(3 downto 0) of STD_LOGIC_VECTOR(15 downto 0);
 signal gen: pp_array0;
 
-type pp_array1 is array(7 downto 0) of STD_LOGIC_VECTOR(3 downto 0);
-signal s1, s2: pp_array1;
-signal sum: pp_array1;
-signal carries : pp_array1;
-signal cout: STD_LOGIC_VECTOR (9 downto 0):= (others=>'0');
 
-type pp_array2 is array(2 downto 0) of STD_LOGIC_VECTOR(3 downto 0);
-signal x1, x2: pp_array2;
-signal sum_final: pp_array2;
-signal carries_final : pp_array2;
-signal cout_final: STD_LOGIC_VECTOR (4 downto 0):= (others=>'0');
+
+
+
+
+signal s1, s2: STD_LOGIC_VECTOR(11 downto 0);
+signal sum: STD_LOGIC_VECTOR(11 downto 0);
+signal carries: STD_LOGIC_VECTOR(11 downto 0);
+
+signal x1, x2: STD_LOGIC_VECTOR(7 downto 0);
+signal x_carry: STD_LOGIC_VECTOR(7 downto 0);
+signal z1, z2: STD_LOGIC_VECTOR(3 downto 0);
+
+
+
 
 
 type pp_comp_array is array(7 downto 0) of STD_LOGIC_VECTOR(1 downto 0);
@@ -49,7 +52,7 @@ signal comp: pp_comp_array;
 
 signal Adder_results1, Adder_results2: STD_LOGIC_VECTOR(1 downto 0);
 signal car: STD_LOGIC_VECTOR(3 downto 0);
-signal car2: STD_LOGIC_VECTOR(1 downto 0);------------------------------------------experiment
+signal car2: STD_LOGIC_VECTOR(1 downto 0);
 
 function approx_HA(a, b : std_logic) return std_logic_vector is
     variable result : std_logic_vector(1 downto 0);
@@ -226,6 +229,7 @@ begin
         gen(j)(8) <= A(7)      when (plus1_out(j) or plus2_out(j)) = '1' else 
                      A_comp(7) when (minus1_out(j) or minus2_out(j)) = '1' else '0';
         gen(j)(15 downto 9) <= (others => gen(j)(8));
+--        gen(j)((14-j*2) downto 9) <= (others => gen(j)(8));
     end generate all_pp_gen;
     
     
@@ -236,87 +240,117 @@ begin
         prod(2) <= Adder_results1(0);
         car(0) <= Adder_results1(1);
  
-   
+ 
     comp(0) <= gen(1)(1) & gen(0)(3);
-          
+      
     --using 32 compressor
     comp(1) <= approx_32(gen(0)(4), gen(1)(2), gen(2)(0));
-    comp(2) <= approx_32(gen(0)(5), gen(1)(3), gen(2)(1));   
+    comp(2) <= approx_32(gen(0)(5), gen(1)(3), gen(2)(1));       
     
     -- using 42 compressor
     pp_gen1: for i in 3 to 7 generate
         comp(i) <= approx_42(gen(0)(i+3), gen(1)(i+1), gen(2)(i-1), gen(3)(i-3));
     end generate;
-   
-   
-    --generating product P(3) to P(10)
-    comp0: for j in 0 to 1 generate  
-        GEN_SUM0: for i in 0 to 3 generate       
-            s1(j)(i) <= comp(i+j*4)(0) and comp(i+j*4)(1);
-            s2(j)(i) <= comp(i+j*4)(0) xor comp(i+j*4)(1);
-        end generate;
-        carry_inst0: CARRY4
-        port map (
-            DI      => s1(j),        
-            S       => s2(j),        
-            O       => sum(j),       
-            CO      => carries(j),   
-            CI      => car(j),  
-            CYINIT  => '0'        
+
+    Type_A: for i in 0 to 7 generate 
+        lut_inst0: LUT6_2 
+        generic map(INIT => X"6000000080000000")
+        port map(
+            I0 => comp(i)(0),
+            I1 => comp(i)(1),
+            I2 => '1',
+            I3 => '1',
+            I4 => '1',
+            I5 => '1',
+            O5 => s1(i),
+            O6 => s2(i)
         );
-        car(j+1) <= carries(j)(3);         --Final carry-out
-        
-        prod0: for i in 0 to 3 generate       
-            prod(i+3+j*4) <= sum(j)(i);
-        end generate;
-    end generate;
-
-
-    car2(0) <= gen(0)(10) and gen(1)(8) and gen(2)(6);  -- for better accuracy
+    end generate Type_A;
+    
+    
+    car2(0) <= gen(0)(10) and gen(1)(8) and gen(2)(6);-- --    previously it was car2(0)<='0'; car2(1)<='0';
     car2(1) <= gen(1)(8) and gen(2)(6) and gen(3)(4);
-
-    --generating product P(12) to P(15)
-    GEN_SUM1: for i in 0 to 3 generate       
-        s1(2)(i) <= gen(0)(i+11) and gen(1)(i+9);
-        s2(2)(i) <= gen(0)(i+11) xor gen(1)(i+9);
-    end generate;
-    GEN_SUM2: for i in 0 to 3 generate       
-        s1(3)(i) <= gen(2)(i+7) and gen(3)(i+5);
-        s2(3)(i) <= gen(2)(i+7) xor gen(3)(i+5);
-    end generate;
-    comp1: for j in 2 to 3 generate 
-        carry_inst1: CARRY4
-        port map (
-            DI      => s1(j),        
-            S       => s2(j),        
-            O       => sum(j),       
-            CO      => carries(j),   
-            CI      => car2(j-2),       
-            CYINIT  => '0'        
-        );  
-    end generate;
     
-    GEN_SUM3: for i in 0 to 3 generate       
-        s1(4)(i) <= sum(2)(i) and sum(3)(i);
-        s2(4)(i) <= sum(2)(i) xor sum(3)(i);
-    end generate;
-    carry_inst2: CARRY4
+    Type_B: for i in 0 to 3 generate 
+        lut_inst1: LUT6_2 
+        generic map(INIT => X"6000000080000000")
+        port map(
+            I0 => gen(0)(i+11),
+            I1 => gen(1)(i+9),
+            I2 => '1',
+            I3 => '1',
+            I4 => '1',
+            I5 => '1',
+            O5 => x1(i),
+            O6 => x2(i));
+    end generate Type_B;
+	carry_inst1: CARRY4                                        
         port map (
-            DI      => s1(4),        
-            S       => s2(4),        
-            O       => sum(4),       
-            CO      => carries(4),   
-            CI      => car(2),       
-            CYINIT  => '0');
-    prod1: for i in 0 to 3 generate       
-        prod(i+11) <= sum(4)(i);
-    end generate;
-
+            DI => x1(3 downto 0),                                              
+            S  => x2(3 downto 0),                                             
+            O  => z1,                                
+            CO => x_carry(3 downto 0),                                        
+            CI => car2(0),                            
+            CYINIT => '0');
+           	
+    Type_C: for i in 0 to 3 generate 
+        lut_inst2: LUT6_2 
+        generic map(INIT => X"6000000080000000")
+        port map(
+            I0 => gen(2)(i+7),
+            I1 => gen(3)(i+5),
+            I2 => '1',
+            I3 => '1',
+            I4 => '1',
+            I5 => '1',
+            O5 => x1(i+4),
+            O6 => x2(i+4)
+        );
+    end generate Type_C;	
+	carry_inst2: CARRY4                                        
+        port map (
+            DI => x1(7 downto 4),                                              
+            S  => x2(7 downto 4),                                             
+            O  => z2,                                
+            CO => x_carry(7 downto 4),                                        
+            CI => car2(1),                            
+            CYINIT => '0'); 
+                       
+    Type_D: for i in 0 to 3 generate 
+        lut_inst3: LUT6_2 
+        generic map(INIT => X"6000000080000000")
+        port map(
+            I0 => z1(i),
+            I1 => z2(i),
+            I2 => '1',
+            I3 => '1',
+            I4 => '1',
+            I5 => '1',
+            O5 => s1(i+8),
+            O6 => s2(i+8)
+        );
+    end generate Type_D;         
+            
+   
+    -- Carry chain implementation
+    carry_chain_A: for z in 0 to 2 generate            
+        carry_inst0: CARRY4                                        
+        port map (
+            DI => s1(z*4+3 downto z*4),                                              
+            S  => s2(z*4+3 downto z*4),                                             
+            O  => sum(z*4+3 downto z*4),                                
+            CO => carries(z*4+3 downto z*4),                                        
+            CI => car(z),                            
+            CYINIT => '0'
+        );
+        car(z+1) <= carries(z*4+3);
+    end generate carry_chain_A;
+    
+    prod(14 downto 3) <= sum;
     prod(15) <= A(7) xor B(7);
-    
 
-	
-	----------- for testbench ------------
+
+    ----------- for testbench ------------
 --	  Scomp0 <= comp(0);
 --    Scomp1 <= comp(1);
 --    Scomp2 <= comp(2);
@@ -325,11 +359,6 @@ begin
 --    Scomp5 <= comp(5);
 --    Scomp6 <= comp(6);
 --    Scomp7 <= comp(7);
---    Ssum0<= sum(0);
---    Ssum1<= sum(1);
---    Ssum2<= sum(2);
---    Ssum3<= sum(3);
---    Ssum4<= sum(4);
 --    Scar<=car;  
 --    Sa_comp <= a_comp;
 ----    Szero_out <= zero_out; 
